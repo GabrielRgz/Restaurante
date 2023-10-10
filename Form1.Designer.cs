e0fc6db88a996e8bec9baf955a24a7334ecf0742
GabrielRgz <80359417+GabrielRgz@users.noreply.github.com>
2023-10-09 19:51:34 -0700

    vKK1.0
    Inicio del proyecto versión KK 1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Restaurante
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblnombre = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblnombre
+            // 
+            lblnombre.AutoSize = true;
+            lblnombre.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            lblnombre.ForeColor = Color.Indigo;
+            lblnombre.Location = new Point(68, 42);
+            lblnombre.Name = "lblnombre";
+            lblnombre.Size = new Size(320, 30);
+            lblnombre.TabIndex = 0;
+            lblnombre.Text = "El crustaceo cascarudo";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.crab;
+            pictureBox1.Location = new Point(49, 118);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(353, 278);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSeaGreen;
+            ClientSize = new Size(439, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblnombre);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblnombre;
+        private PictureBox pictureBox1;
+    }
+}
