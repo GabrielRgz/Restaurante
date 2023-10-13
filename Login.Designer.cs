20d4e55342e657719c7e2776d0e622359cf3b673
GabrielRgz <80359417+GabrielRgz@users.noreply.github.com>
2023-10-12 20:06:04 -0700

    Ventana login
    diseño basico

---
+++ Login.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Restaurante
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txbUser = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblnombre.ForeColor = System.Drawing.Color.Indigo;
+            this.lblnombre.Location = new System.Drawing.Point(70, 24);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(320, 30);
+            this.lblnombre.TabIndex = 0;
+            this.lblnombre.Text = "El crustaceo cascarudo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Restaurante.Properties.Resources.crab;
+            this.pictureBox1.Location = new System.Drawing.Point(111, 93);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(203, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(123, 252);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(47, 15);
+            this.lblUser.TabIndex = 2;
+            this.lblUser.Text = "Usuario";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(125, 321);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(67, 15);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Contraseña";
+            // 
+            // txbUser
+            // 
+            this.txbUser.Location = new System.Drawing.Point(126, 281);
+            this.txbUser.Name = "txbUser";
+            this.txbUser.Size = new System.Drawing.Size(188, 23);
+            this.txbUser.TabIndex = 4;
+            this.txbUser.Text = "Nombre de usuario";
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(128, 348);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(186, 23);
+            this.txbPassword.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(129, 388);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(185, 23);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Iniciar sesion";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(130, 432);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(184, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(475, 498);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.txbUser);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblnombre);
+            this.Name = "Login";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblnombre;
+        private PictureBox pictureBox1;
+        private Label lblUser;
+        private Label lblPassword;
+        private TextBox txbUser;
+        private TextBox txbPassword;
+        private Button btnLogin;
+        private Button btnSalir;
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,20 @@
+namespace Restaurante
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //Mandar a ventana principal
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            //cerrar el programa
+        }
+    }
+}
